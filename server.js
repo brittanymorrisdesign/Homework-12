e0474ca41d0b00e3b14c634b0d66bec749bc327b
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-17 21:38:54 -0500

    Added inquirer prompts and functions

--- server.js
+++ server.js
@@ -25,3 +25,67 @@ connection.connect(function(err) {
     console.log(res);
   });
 });
+
+// Initial Prompt
+function runSearch() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'View All Employees by Department',
+        'View All Employees by Manager',
+        'Add Employee',
+        'Remove Employee',
+        'Update Employee Role',
+        'Update Employee Manager',
+        'View all Roles',
+      ],
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+        case 'View All Employees':
+          viewAllEmployees();
+          break;
+
+        case 'View All Employees by Department':
+          viewEmployeesByDept();
+          break;
+
+        case 'View All Employees by Manager':
+          viewEmployeesByManager();
+          break;
+
+        case 'Add Employee':
+          addEmployee();
+          break;
+
+        case 'Remove Employee':
+          removeEmployee();
+          break;
+
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+
+        case 'Update Employee Manager':
+          updateEmployeeManager();
+          break;
+
+        case 'View all Roles':
+          viewAllRoles();
+          break;
+      }
+    });
+}
+
+function viewAllEmployees() {}
+function viewEmployeesByDept() {}
+function viewEmployeesByManager() {}
+function addEmployee() {}
+function removeEmployee() {}
+function updateEmployeeRole() {}
+function updateEmployeeManager() {}
+function viewAllRoles() {}
